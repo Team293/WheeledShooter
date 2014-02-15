aaa36d52400943b9edeabc7a71c2fc2b80349fdd
Peter Mitrano <mitranopeter@gmail.com>
2014-02-14 23:38:30 -0500

    SUCCESSS
    toggle feed with A button
    pass by holding b button
    watch out for ball limit, must be adjusted correctly
    still doesn't have cage release
    no auto aim, distance, autonomous
    port numbers are correct
    includes driving reversal and firing
    bottom wheel now should start in the right direction, and trigger should
    start up

--- WheeledShooter/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ WheeledShooter/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -7,10 +7,12 @@
 package edu.wpi.first.wpilibj.templates;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SpikeEncoder;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.buttons.SpikeButton;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
@@ -31,41 +33,50 @@ public class RobotTemplate extends IterativeRobot {
      */
     Talon leftMotor = new Talon(Ports.leftDrive),
             rightMotor = new Talon(Ports.rightDrive);
+    RobotDrive drive = new RobotDrive(leftMotor, rightMotor);
 
     Talon shooterLow = new Talon(Ports.shooterLow),
             shooterMiddle = new Talon(Ports.shooterMiddle),
             shooterHigh = new Talon(Ports.shooterHigh);
 
     Relay trigger = new Relay(Ports.trigger),
-            feederMotor = new Relay(Ports.feeder);
-    RobotDrive drive = new RobotDrive(leftMotor, rightMotor);
+            feederMotor = new Relay(Ports.feeder),
+            feederMotor2 = new Relay(Ports.feeder2);
 
     Joystick leftJoystick = new Joystick(Ports.leftJoystick),
             rightJoystick = new Joystick(Ports.rightJoystick),
             gamepad = new Joystick(Ports.gamepad);
 
-//    SpikeEncoder enc1 = new SpikeEncoder(Ports.shooterLowEncA, Ports.shooterLowEncB),
-//            enc2 = new SpikeEncoder(Ports.shooterMiddleEncA, Ports.shooterMiddleEncB),
-//            enc3 = new SpikeEncoder(Ports.shooterHighEncA, Ports.shooterHighEncB);
+    SpikeEncoder enc1 = new SpikeEncoder(Ports.shooterLowEncA, Ports.shooterLowEncB),
+            enc2 = new SpikeEncoder(Ports.shooterMiddleEncA, Ports.shooterMiddleEncB),
+            enc3 = new SpikeEncoder(Ports.shooterHighEncA, Ports.shooterHighEncB);
+
     SpikeButton pass = new SpikeButton(gamepad, Ports.pass),
             toggleFeeder = new SpikeButton(gamepad, Ports.toggleFeeder),
             fire = new SpikeButton(rightJoystick, Ports.fire),
             toggleDriveDirection = new SpikeButton(rightJoystick, Ports.toggleDriveDirection);
 
     DigitalInput ballLimit = new DigitalInput(Ports.ballLimit);
+
+    Gyro gyro = new Gyro(Ports.gyro);
+
+//    AnalogChannel leftUltrasonic = new AnalogChannel(Ports.leftUltrasonic);
+//    AnalogChannel rightUltrasonic = new AnalogChannel(Ports.rightUltrasonic);
+//    DigitalOutput ultrasonicSignal = new DigitalOutput(Ports.ultrasonicSignal);
     boolean shooting = false;
 
     public void robotInit() {
         addComponents();
-        //enc1.start();
-        //enc2.start();
-        //enc3.start();
+        enc1.start();
+        enc2.start();
+        enc3.start();
     }
 
     public void teleopInit() {
-        SmartDashboard.putNumber("1", 0.4);
+        SmartDashboard.putNumber("1", -0.4);
         SmartDashboard.putNumber("2", 0.4);
         SmartDashboard.putNumber("3", 0.4);
+        trigger.set(Relay.Value.kForward);
     }
 
     /**
@@ -86,9 +97,9 @@ public class RobotTemplate extends IterativeRobot {
         double speed1 = SmartDashboard.getNumber("1", 0.0);
         double speed2 = SmartDashboard.getNumber("2", 0.0);
         double speed3 = SmartDashboard.getNumber("3", 0.0);
-//        SmartDashboard.putNumber("enc1 RPM", enc1.getRPM());
-//        SmartDashboard.putNumber("enc2 RPM", enc2.getRPM());
-//        SmartDashboard.putNumber("enc3 RPM", enc3.getRPM());
+        SmartDashboard.putNumber("enc1 RPM", enc1.getRPM());
+        SmartDashboard.putNumber("enc2 RPM", enc2.getRPM());
+        SmartDashboard.putNumber("enc3 RPM", enc3.getRPM());
 
         if (toggleDriveDirection.getState()) {
             drive.tankDrive(leftJoystick.getY(), rightJoystick.getY());
@@ -108,24 +119,31 @@ public class RobotTemplate extends IterativeRobot {
         if (!shooting) {
             if (pass.get()) {
                 feederValue = Relay.Value.kReverse;
+                SmartDashboard.putString("feeder value", "passing!");
             } else if (toggleFeeder.getState()) {
-                if (!ballLimit.get()) {
+                //ball limit is wired in reverse!~~~
+                if (ballLimit.get()) {
                     feederValue = Relay.Value.kForward;
+                    SmartDashboard.putString("feeder value", "feeding!");
                 } else {
                     feederValue = Relay.Value.kOff;
+                    SmartDashboard.putString("feeder value", "stopped!");
                 }
             } else {
                 feederValue = Relay.Value.kOff;
+                SmartDashboard.putString("feeder value", "off state!");
             }
         } else {
             trigger.set(Relay.Value.kOff);
             feederValue = Relay.Value.kForward;
-            if (!ballLimit.get()) {
+            if (ballLimit.get()) {
                 shooting = false;
                 trigger.set(Relay.Value.kForward);
             }
         }
+        SmartDashboard.putString("final feederValue", feederValue + "");
         feederMotor.set(feederValue);
+        feederMotor2.set(feederValue);
     }
 
     /**
@@ -139,11 +157,12 @@ public class RobotTemplate extends IterativeRobot {
         LiveWindow.addActuator("shooter rack", "shooter 1", shooterLow);
         LiveWindow.addActuator("shooter rack", "shooter 2", shooterMiddle);
         LiveWindow.addActuator("shooter rack", "shooter 3", shooterHigh);
-//        LiveWindow.addSensor("shooter rack", "shooter enc 1", enc1);
-//        LiveWindow.addSensor("shooter rack", "shooter enc 2", enc2);
-//        LiveWindow.addSensor("shooter rack", "shooter enc 3", enc3);
+        LiveWindow.addSensor("shooter rack", "shooter enc 1", enc1);
+        LiveWindow.addSensor("shooter rack", "shooter enc 2", enc2);
+        LiveWindow.addSensor("shooter rack", "shooter enc 3", enc3);
         LiveWindow.addActuator("trigger", "trigger", trigger);
         LiveWindow.addSensor("trigger", "ball limit", ballLimit);
+        LiveWindow.addSensor("gyro", "gyro", gyro);
         LiveWindow.addActuator("feeder", "feeder", feederMotor);
     }
 }
